42c6011594f430d5614ad62d006c39164f03f25e
Denis Aganov <den.aganov@yandex.ru>
2022-05-18 21:27:59 +0300

    New task

--- TaskManagment/View/NewTask.swift
+++ TaskManagment/View/NewTask.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct NewTask: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    // MARK: - Task Values
+    @State var taskTitle = ""
+    @State var taskDescription = ""
+    @State var taskDate = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationView {
+            List {
+                
+                Section {
+                    TextField("Go to work", text: $taskTitle)
+                } header: {
+                    Text("Заголовок")
+                }
 
-struct NewTask_Previews: PreviewProvider {
-    static var previews: some View {
-        NewTask()
+                Section {
+                    TextField("Nothing", text: $taskDescription)
+                } header: {
+                    Text("Описание")
+                }
+
+                Section {
+                    DatePicker("", selection: $taskDate)
+                        .datePickerStyle(.graphical)
+                        .labelsHidden()
+                } header: {
+                    Text("Дата")
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Новая задача")
+            .navigationBarTitleDisplayMode(.inline)
+            // MARK: Disabling dismiss on Swipe
+            .interactiveDismissDisabled()
+            // MARK: Action Buttons
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Сохранить") {
+                        
+                    }
+                    .disabled(taskTitle == "" || taskDescription == "")
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Отмена") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
